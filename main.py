49bd33b90a590105ea1d9b6e615b1499b8c3ea19
Ashish Raj <ashish1195.raj@gmail.com>
2025-08-16 10:56:55 +0530

    prediction completed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from pydantic import BaseModel,Field,computed_field
 from typing import Literal,Annotated
 
-with open ('model.pkl','rd') as f:
+with open ('model.pkl','rb') as f:
     model= pickle.load(f)
 
 app=FastAPI()
@@ -25,5 +25,61 @@ class UserInput(BaseModel):
     age: Annotated[int,Field(...,gt=0,lt=120, description="Age of the user")]
     weight: Annotated[float, Field(...,gt=0,description="weight of the user")]
     height: Annotated[float,Field(...,gt=0,lt=2.5, description="height of the user")]
+    income_lpa: Annotated[float,Field(...,gt=0,description="Annual Salary of the user in lap")]
+    smoker: Annotated[bool,Field(...,description="is user a smoker")]
+    city: Annotated[str, Field(...,description="the city that the user belong to ")]
+    occupation: Annotated[Literal['retired', 'freelancer', 'student', 'government_job',
+       'business_owner', 'unemployed', 'private_job'],Field(...,description="Occupation of the user")]
     
+    
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
+    
+    @computed_field
+    @property
+    def age_group(self) -> str:
+        if self.age < 25:
+            return "young"
+        elif self.age < 45:
+            return "adult"
+        elif self.age < 60:
+            return "middle_age"
+        return "senior"
 
+    @computed_field
+    @property
+    def lifestyle_risk(self)-> str:
+        if self.smoker and self.bmi > 30:
+            return "high"
+        elif self.smoker or self.bmi > 27:
+            return "medium"
+        else:
+            return "low"
+    
+    @computed_field
+    @property
+    def city_tire(self)-> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+
+@app.post('/predict')
+def predic_premium(data: UserInput):
+    
+    input_df=pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group':data.age_group,
+        'lifestyle_risk':data.lifestyle_risk,
+        'city_tire': data.city_tire,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+
+    prediction= model.predict(input_df)[0]
+    
+    return JSONResponse(status_code=200, content={'predicted_category':prediction})
